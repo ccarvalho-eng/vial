f00998bef9016aef93dde5afcb7d1fae72db6054
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2026-03-23 15:02:35 -0300

    refactor: remove Run button from suite index
    Users should navigate to suite detail page for full control over
    version and provider selection before running.

--- lib/vial_web/live/suite_live/index.ex
+++ lib/vial_web/live/suite_live/index.ex
@@ -22,7 +22,6 @@ defmodule VialWeb.SuiteLive.Index do
       socket
       |> assign(:page_title, "Suites")
       |> assign(:suites, suites)
-      |> assign(:running_suite_id, nil)
 
     {:noreply, socket}
   end
@@ -36,52 +35,5 @@ defmodule VialWeb.SuiteLive.Index do
      socket
      |> assign(:suites, Evals.list_suites_with_prompt())
      |> put_flash(:info, "Suite deleted successfully")}
-  end
-
-  @impl Phoenix.LiveView
-  def handle_event("run_suite", %{"id" => id}, socket) do
-    suite = Evals.get_suite_with_test_cases_and_prompt!(id)
-    prompt = Vial.Prompts.get_prompt_with_versions!(suite.prompt_id)
-    providers = Vial.Providers.list_providers()
-
-    case {prompt.versions, providers} do
-      {[], _} ->
-        {:noreply, put_flash(socket, :error, "No prompt versions available")}
-
-      {_, []} ->
-        {:noreply, put_flash(socket, :error, "No providers configured")}
-
-      {[version | _], [provider | _]} ->
-        # Start async execution
-        pid = self()
-        suite_id = suite.id
-
-        Task.Supervisor.start_child(Vial.TaskSupervisor, fn ->
-          suite = Evals.get_suite_with_test_cases_and_prompt!(suite_id)
-          result = Evals.execute_suite(suite, version, provider)
-          send(pid, {:suite_run_completed, suite_id, result})
-        end)
-
-        {:noreply, assign(socket, :running_suite_id, suite.id)}
-    end
-  end
-
-  @impl Phoenix.LiveView
-  def handle_info({:suite_run_completed, suite_id, {:ok, _suite_run}}, socket) do
-    suite = Evals.get_suite_with_prompt!(suite_id)
-
-    {:noreply,
-     socket
-     |> assign(:running_suite_id, nil)
-     |> put_flash(:info, "Suite '#{suite.name}' executed successfully")
-     |> push_navigate(to: ~p"/suites/#{suite_id}")}
-  end
-
-  @impl Phoenix.LiveView
-  def handle_info({:suite_run_completed, _suite_id, {:error, _reason}}, socket) do
-    {:noreply,
-     socket
-     |> assign(:running_suite_id, nil)
-     |> put_flash(:error, "Failed to execute suite")}
   end
 end
