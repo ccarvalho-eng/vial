2a79498cf729cad16e91b8fe3428d486e0c41eb0
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2026-03-24 15:54:47 -0300

    feat(layouts): add root layout for embedded dashboard

---
+++ lib/vial/web/layouts.ex
@@ -0,0 +1,28 @@
+defmodule Vial.Web.Layouts do
+  @moduledoc false
+  use Phoenix.Component
+
+  embed_templates "layouts/*"
+
+  def root(assigns) do
+    ~H"""
+    <!DOCTYPE html>
+    <html lang="en" class="h-full bg-gray-100">
+      <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Vial Dashboard</title>
+        <link rel="stylesheet" href={~p"/css-#{asset_hash(:css)}"} />
+        <script src={~p"/js-#{asset_hash(:js)}"} defer>
+        </script>
+      </head>
+      <body class="h-full">
+        {@inner_content}
+      </body>
+    </html>
+    """
+  end
+
+  defp asset_hash(:css), do: "TODO"
+  defp asset_hash(:js), do: "TODO"
+end
