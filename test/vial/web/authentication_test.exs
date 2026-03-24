ce70ec1535f566cb2e6bdb78b002fd0e0d780b0e
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2026-03-24 15:34:58 -0300

    test(auth): add comprehensive test for all session assigns

--- test/vial/web/authentication_test.exs
+++ test/vial/web/authentication_test.exs
@@ -21,5 +21,33 @@ defmodule Vial.Web.AuthenticationTest do
       assert updated_socket.assigns.refresh == 10
       assert updated_socket.assigns.user == %{id: 1}
     end
+
+    test "assigns all session values to socket" do
+      session = %{
+        "access" => :all,
+        "refresh" => 5,
+        "user" => %{id: 2, name: "Admin"},
+        "resolver" => MyApp.Resolver,
+        "vial_name" => :vial,
+        "prefix" => "/admin",
+        "logo_path" => "/logo.png",
+        "csp_nonces" => %{img: "abc", style: "def", script: "ghi"}
+      }
+
+      socket = %Phoenix.LiveView.Socket{
+        private: %{connect_params: %{}, connect_info: %{session: session}}
+      }
+
+      {:cont, updated_socket} = Authentication.on_mount(:default, %{}, session, socket)
+
+      assert updated_socket.assigns.access == :all
+      assert updated_socket.assigns.refresh == 5
+      assert updated_socket.assigns.user == %{id: 2, name: "Admin"}
+      assert updated_socket.assigns.resolver == MyApp.Resolver
+      assert updated_socket.assigns.vial_name == :vial
+      assert updated_socket.assigns.prefix == "/admin"
+      assert updated_socket.assigns.logo_path == "/logo.png"
+      assert updated_socket.assigns.csp_nonces == %{img: "abc", style: "def", script: "ghi"}
+    end
   end
 end
