94d249581622c8d69a30908268b9f5412c5cee65
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2026-03-28 20:50:50 -0300

    chore: bump version to 0.1.7

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Aludel.MixProject do
   def project do
     [
       app: :aludel,
-      version: "0.1.6",
+      version: "0.1.7",
       elixir: "~> 1.19",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
