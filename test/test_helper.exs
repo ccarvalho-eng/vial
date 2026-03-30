fea49491143c64213132e7cf530851e3b846bc47
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2026-03-30 18:21:21 -0300

    test: remove unused integration test exclusions

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -73,8 +73,4 @@ Mox.defmock(Aludel.Interfaces.HttpClientMock,
 # Configure test environment to use mocked HTTP client
 Application.put_env(:aludel, :http_client, Aludel.Interfaces.HttpClientMock)
 
-# Exclude integration tests that require external services or real API keys
-# Run with: mix test --include ollama (if you have Ollama running)
-# Run with: mix test --include anthropic_integration (requires ANTHROPIC_API_KEY)
-# Run with: mix test --include openai_integration (requires OPENAI_API_KEY)
-ExUnit.start(exclude: [:ollama, :anthropic_integration, :openai_integration])
+ExUnit.start()
